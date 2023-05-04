cfbcc0a0fe1f7da2f73f0dd713ca26edb1b62509
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-05-04 19:36:24 +0545

    .

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export  {Header} from './Header';
-export  {Footer} from './Footer';
-export  {Card} from './Card';
+export { Header } from './Header';
+export { Footer } from './Footer';
+export { Card } from './Card';
