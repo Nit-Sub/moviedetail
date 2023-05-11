6cf055ae6ed492a31803d9ef06fe5807a0027a00
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-05-11 19:56:11 +0545

    .

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 export const Card = () => {
   return (
-    <div>Card</div>
+    <>
+    </>
   )
 }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -4,7 +4,7 @@ import {Header} from '../components/Header'
 export const MovieList = () => {
   return (
    <>
-   
+   <div>Movie list</div>
    </>
   )
 }
