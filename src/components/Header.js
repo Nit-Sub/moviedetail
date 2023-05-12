f9cacddba726287cd931ca187e9f76d96924a4ca
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-05-12 17:41:24 +0545

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import logo from '../assests/logo.png'
-import { Link, NavLink, json } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 export const Header = () => {
     const activeClass = "text-base block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500";
     const inActiveClass = "text-base block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
