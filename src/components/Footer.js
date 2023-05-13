bd17333e40431cf84e1d0b7c5868078fd94a4275
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-05-13 21:59:19 +0545

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,16 +11,16 @@ export const Footer = () => {
                     </span>
                     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                         <li>
-                            <a href="https://www.instagram.com/nite_sh__/" target="_blank"className="mr-4 hover:underline md:mr-6 ">Instagram</a>
+                            <a href="https://www.instagram.com/nite_sh__/" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
                         </li>
                         <li>
-                            <a href="https://www.facebook.com/nitesh.subedi.3" target="_blank"className="mr-4 hover:underline md:mr-6">Facebook</a>
+                            <a href="https://www.facebook.com/nitesh.subedi.3" className="mr-4 hover:underline md:mr-6">Facebook</a>
                         </li>
                         <li>
-                            <a href="#" className="mr-4 hover:underline md:mr-6">Twitter</a>
+                            <a href="https://www.facebook.com/nitesh.subedi.3" className="mr-4 hover:underline md:mr-6">Twitter</a>
                         </li>
                         <li>
-                            <a href="https://www.youtube.com/channel/UCUdNmvXrL5Vzra6RfJ80VhQ" target="_blank"className="hover:underline">Youtube</a>
+                            <a href="https://www.youtube.com/channel/UCUdNmvXrL5Vzra6RfJ80VhQ" className="hover:underline">Youtube</a>
                         </li>
                     </ul>
                 </div>
