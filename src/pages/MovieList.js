e32230d862feb5615fef2763f8d6d9c85dc6cba8
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-05-13 09:50:02 +0545

    Update MovieList.js

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -4,12 +4,9 @@ import { Card } from '../components/Card'
 export const MovieList = () => {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
-    async function fetchMovies() {
-      const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=fcc6d85d62f934ad475896c695d21990     ")
-      const data = await response.json();
-      setMovies(data.results);
-    }
-    fetchMovies();
+   
+    
+    
 
   }, [])
   return (
@@ -17,8 +14,7 @@ export const MovieList = () => {
       <main>
         <section className='max-w-7xl mx-auto py-7'>
           <div className='flex justify-start flex-wrap'>
-            {movies.map((movie) =>
-              <Card />)}
+           
           </div>
         </section>
       </main>
