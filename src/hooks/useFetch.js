9fc67f1a914beb4c9e42c76e2db9a63536decf37
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-05-13 16:03:00 +0545

    .

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,7 @@
 import { useState , useEffect } from 'react'
-const useFetch = (apiPath) => {
+const useFetch = (apiPath,queryTerm="") => {
     const [data ,setData]=useState([])
-    const url=`https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}`
+    const url=`https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`
     useEffect(() => {
         async function fetchMovies() {
           const response = await fetch(url)
